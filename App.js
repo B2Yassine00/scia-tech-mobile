f24a79e0e23b41d9c1ab3bc5611992bbbebb9fc8
B2Yassine00 <banouq90@gmail.com>
2023-08-20 18:18:41 +0100

    first commit

--- App.js
+++ App.js
@@ -1,21 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, Text, Alert, View } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <View className="flex-1 items-center justify-center bg-[#05668D]">
+      <Text className="text-white">Open up App.js to start working on your app!</Text>
+      <Button title="Press me" onPress={() => Alert.alert('Simple Button pressed')}/>
       <StatusBar style="auto" />
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
